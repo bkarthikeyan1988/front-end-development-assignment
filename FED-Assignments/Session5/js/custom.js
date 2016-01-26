ad18adf818cd05970ebeb5fa4aa552113638c1af
karthikeyan_b <bkarthikeyan.bk@gmail.com>
2016-01-26 20:53:22 +0530

    For FED Session 5 Assignment 3 and 4

--- FED-Assignments/Session5/js/custom.js
+++ FED-Assignments/Session5/js/custom.js
@@ -2,13 +2,27 @@ $(document).ready(function() {
 
     // For Session 5 Assignment 1 Starts
 
-    $( "div" ).find( "h1" ).css( "background-color", "#ccc" );
+        $( "div" ).find( "h1" ).css( "background-color", "#ccc" );
 
     // For Session 5 Assignment 1 Ends
 
     // For Session 5 Assignment 2 Starts
 
-    $( "form" ).find( "input" ).hide();
+        $( "form" ).find( "input" ).hide();
+
+    // For Session 5 Assignment 2 Ends
+
+    // For Session 5 Assignment 3 Starts
+
+        $( "li" ).each(function(index) {
+            $(this).prepend("<strong>"+index+":</strong>");
+        });
+
+    // For Session 5 Assignment 3 Ends
+
+    // For Session 5 Assignment 2 Starts
+
+        $("#acadgild").attr("href","http://www.google.com/").html("Google");
 
     // For Session 5 Assignment 2 Ends
 });
