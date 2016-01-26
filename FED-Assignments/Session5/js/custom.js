439a1146c571fcc005b22ad30ef59823e5dbe7a8
karthikeyan_b <bkarthikeyan.bk@gmail.com>
2016-01-26 19:15:18 +0530

    For Session 5 Assignment 1 and 2

---
+++ FED-Assignments/Session5/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    // For Session 5 Assignment 1 Starts
+
+    $( "div" ).find( "h1" ).css( "background-color", "#ccc" );
+
+    // For Session 5 Assignment 1 Ends
+
+    // For Session 5 Assignment 2 Starts
+
+    $( "form" ).find( "input" ).hide();
+
+    // For Session 5 Assignment 2 Ends
+});
