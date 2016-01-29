fe4db17218669a401940baf979e81805239669e7
karthikeyan_b <bkarthikeyan.bk@gmail.com>
2016-01-29 18:27:15 +0530

    FED Session 3 Assignment 2

---
+++ FED-Assignments/Session3/js/custom.js
@@ -0,0 +1,19 @@
+x = 6;
+y = 3;
+
+document.getElementById("addBtn").onclick = function add() {
+	var z= x + y;
+  	alert(z);
+}
+document.getElementById("subBtn").onclick = function sub() {
+	var z= x - y;
+  	alert(z);
+}
+document.getElementById("multiplyBtn").onclick = function multiply() {
+	var z= x * y;
+  	alert(z);
+}
+document.getElementById("divBtn").onclick = function div() {
+	var z= x / y;
+  	alert(z);
+}
